418a9c63e1fde017b5c32b1a337f556e441c4204
Sergei Melnikov <melnikhov@gmail.com>
2022-03-14 23:03:03 +0300

    Added solution for task 2.48

---
+++ 2.48/prog.pas
@@ -0,0 +1,24 @@
+program task_2_48;
+var
+    f: text;
+    c: char;
+    counter: integer = 0;
+begin
+    {$I-}
+    if ParamCount < 1 then begin
+        writeln(ErrOutput, 'No file path');
+        halt(1)
+    end;
+    assign(f, ParamStr(1));
+    reset(f);
+    if IOResult <> 0 then begin
+        writeln(ErrOutput, 'Couldn''t read file');
+        halt(1)
+    end;
+    while not seekeof(f) do begin
+        while not eoln(f) do
+            read(f, c);
+        counter := counter + 1
+    end;
+    writeln(counter)
+end.
