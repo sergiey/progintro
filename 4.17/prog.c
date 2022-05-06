021d8404de300709a27935a9c87d85242184be16
Sergei Melnikov <melnikhov@gmail.com>
2022-05-06 20:53:48 +0300

    Added recursive solution for the first part of the task 4.17

--- 4.17/prog.c
+++ 4.17/prog.c
@@ -12,11 +12,11 @@ struct item* int_array_to_list(const int *arr, int len)
     struct item *first = NULL, *last = NULL, *ptr;
     for(i = 0; i < len; i++) {
         ptr = malloc(sizeof(struct item));
-        ptr->x = arr[i];
-        ptr->next = NULL;
+        ptr -> x = arr[i];
+        ptr -> next = NULL;
         if(first) {
-            last->next = ptr;
-            last = last->next;  
+            last -> next = ptr;
+            last = last -> next;  
         } else {
             first = last = ptr;
         }
@@ -24,16 +24,28 @@ struct item* int_array_to_list(const int *arr, int len)
     return first;
 }
 
+struct item* int_array_to_list_rec(const int *arr, int len)
+{
+    struct item *ptr;
+    if(len == 0)
+        return NULL;
+    ptr = malloc(sizeof(struct item));
+    ptr -> x = *arr;
+    ptr -> next = int_array_to_list_rec(++arr, --len);
+    return ptr;
+}
+
 void print_list(const struct item *list)
 {
-    for(; list; list = list->next)
-        printf("%d ", list->x);
+    for(; list; list = list -> next)
+        printf("%d ", list -> x);
 }
 
 int main()
 {
-    int arr[] = { 1, 5, 7, 9, 15 };
-    struct item *ptr = int_array_to_list(arr, 5);
+    int arr[] = { 1, 3, 5, 7, 13, 15 };
+    int arrlen = sizeof(arr) / sizeof(arr[0]);
+    struct item *ptr = int_array_to_list_rec(arr, arrlen);
     print_list(ptr);
     printf("\n");
     return 0;
