843a7f05ce70f1654383c2aed3b892a93f4384bf
Sergei Melnikov <melnikhov@gmail.com>
2022-05-24 23:25:49 +0300

    Added solution for the second part of task 4.17

--- 4.17/prog.c
+++ 4.17/prog.c
@@ -12,11 +12,11 @@ struct item* int_array_to_list(const int *arr, int len)
     struct item *first = NULL, *last = NULL, *ptr;
     for(i = 0; i < len; i++) {
         ptr = malloc(sizeof(struct item));
-        ptr -> x = arr[i];
-        ptr -> next = NULL;
+        ptr->x = arr[i];
+        ptr->next = NULL;
         if(first) {
-            last -> next = ptr;
-            last = last -> next;  
+            last->next = ptr;
+            last = last->next;  
         } else {
             first = last = ptr;
         }
@@ -30,15 +30,38 @@ struct item* int_array_to_list_rec(const int *arr, int len)
     if(len == 0)
         return NULL;
     ptr = malloc(sizeof(struct item));
-    ptr -> x = *arr;
-    ptr -> next = int_array_to_list_rec(++arr, --len);
+    ptr->x = *arr;
+    ptr->next = int_array_to_list_rec(++arr, --len);
     return ptr;
 }
 
 void print_list(const struct item *list)
 {
-    for(; list; list = list -> next)
-        printf("%d ", list -> x);
+    for(; list; list = list->next)
+        printf("%d ", list->x);
+}
+
+int* int_list_to_array(struct item *list)
+{
+    struct item *list_copy = list;
+    int len = 0;
+    for(; list; list = list->next)
+        len++;
+    int *arr = malloc((len + 1) * sizeof(int));
+    arr[0] = len;
+    int i;
+    for(i = 1; i <= len; i++) {
+        arr[i] = list_copy->x;
+        list_copy = list_copy->next;
+    }
+    return arr;
+}
+
+void print_int_array(int *arr)
+{
+    int i;
+    for(i = 1; i <= arr[0]; i++)
+        printf("%d ", arr[i]);
 }
 
 int main()
@@ -47,6 +70,9 @@ int main()
     int arrlen = sizeof(arr) / sizeof(arr[0]);
     struct item *ptr = int_array_to_list_rec(arr, arrlen);
     print_list(ptr);
+    printf("\n");
+    int* arr2 = int_list_to_array(ptr);
+    print_int_array(arr2);
     printf("\n");
     return 0;
 }
