5763852a6461380342001575a9cd3d903d816298
Sergei Melnikov <melnikhov@gmail.com>
2022-05-31 14:22:58 +0300

    Added recursive solution for the second part of task 4.17

--- 4.17/prog.c
+++ 4.17/prog.c
@@ -64,6 +64,36 @@ void print_int_array(int *arr)
         printf("%d ", arr[i]);
 }
 
+int get_array_element_number()
+{
+    static int x = 1;
+    return x++;
+}
+
+int get_list_length(struct item *list)
+{
+    int len = 0;
+    for(; list; list = list->next)
+        len++;
+    return len;
+}   
+
+int* int_list_to_array_rec(struct item *list)
+{
+    int pos = get_array_element_number();
+    static int *arr;
+    if(pos == 1) {
+        int len = get_list_length(list);
+        arr = malloc((len + 1) * sizeof(int));
+        arr[0] = len;
+        arr[1] = list->x;
+    } else
+        arr[pos] = list->x;
+    if(list->next)
+        int_list_to_array_rec(list->next);
+    return arr;
+}
+
 int main()
 {
     int arr[] = { 1, 3, 5, 7, 13, 15 };
@@ -71,7 +101,7 @@ int main()
     struct item *ptr = int_array_to_list_rec(arr, arrlen);
     print_list(ptr);
     printf("\n");
-    int* arr2 = int_list_to_array(ptr);
+    int* arr2 = int_list_to_array_rec(ptr);
     print_int_array(arr2);
     printf("\n");
     return 0;
