20c5b8ed3094cea742ecae52b5afbd00cf5c39e2
Sergei Melnikov <melnikhov@gmail.com>
2022-06-01 21:43:06 +0300

    Added some functions for task 4.20 (2.42)

---
+++ 4.20/2.42/prog.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* init_char_array(int array_len)
+{
+    return malloc(array_len * sizeof(char));
+}
+
+int add_char_to_array(char c, char **array_ptr)
+{
+    static int content_len = 0;
+    static int array_len = 5;
+    if(!*array_ptr)
+        *array_ptr = init_char_array(array_len);
+    if(array_len <= content_len) {
+        char *tmp = *array_ptr;
+        array_len *= 2;
+        *array_ptr = init_char_array(array_len);
+        for(int i = 0; i < content_len; i++)
+            (*array_ptr)[i] = tmp[i];
+        free(tmp);
+    }
+    (*array_ptr)[content_len] = c;
+    return content_len++;
+}
+
+void print_char_array(char *array, int array_len)
+{
+    int i;
+    for(i = 0; i < array_len; i++)
+        printf("%c", array[i]);
+}
+
+int main()
+{
+    char c;
+    int content_len = 0;
+    char *arr = NULL;
+    while((c = getchar()) != EOF)
+        content_len = add_char_to_array(c, &arr);
+    print_char_array(arr, content_len);
+    printf("\n");
+    return 0;
+}
