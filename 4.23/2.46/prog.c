35c547db9cb756e895a52ec28f78366888a6af9c
Sergei Melnikov <melnikhov@gmail.com>
2022-06-05 11:42:03 +0300

    Added solution for task 4.23 (2.46)

---
+++ 4.23/2.46/prog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TEXT \
+    "Humpty Dumpty sat on a wall\n" \
+    "Humpty Dumpty had a great fall\n" \
+    "All the king's horses and all the king's men\n" \
+    "Couldn't put Humpty together again\n"
+
+int main(int argc, char **argv)
+{
+    if(argc < 2) {
+        fprintf(stderr, "File address not specified\n");
+        exit(1);
+    }
+    FILE *f;
+    f = fopen(argv[1], "w");
+    if(!f) {
+        perror(argv[1]);
+        exit(2);
+    }
+    fprintf(f, TEXT);
+    return 0;
+}
