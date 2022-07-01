44093222bfba7e4a1d0095780b725aa5b74d0bbe
Sergei Melnikov <melnikhov@gmail.com>
2022-07-01 14:23:10 +0300

    Added solution for task 5.05

---
+++ 5.05/prog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv)
+{
+    int f, len, val, i, div, rem;
+    long pos;
+    enum { bufsize = 4096 };
+    unsigned char buf[bufsize];
+    if(argc < 5) {
+        fprintf(stderr, "Four parameters required\n");
+        return 1;
+    }
+    f = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC);
+    if(f == -1) {
+        perror(argv[1]);
+        return 2;
+    }
+    sscanf(argv[3], "%d", &len);
+    sscanf(argv[4], "%d", &val);
+    sscanf(argv[2], "%ld", &pos);
+    for(i = 0; i < bufsize; i++)
+        buf[i] = val;
+    printf("start = %ld, len = %d, val = %d \n", pos, len, val);
+    lseek(f, pos, SEEK_SET);
+    if(len <= bufsize)
+        write(f, &buf, len);
+    else {
+        div = len /bufsize;
+        rem = len % bufsize;
+        for(i = 0; i < div; i++)
+            write(f, &buf, bufsize);
+        write(f, &buf, rem);
+    }
+    close(f);
+    return 0;
+}
