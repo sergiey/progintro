4349ccd8fac7b106f65e60b6ca2637adac64b02f
sergmelnikov <melnikhov@gmail.com>
2022-09-07 20:20:31 +0300

    Added solution for task 10.02.

---
+++ c_sharp/Progintro.Part10/Task10.02/B.cs
@@ -0,0 +1,21 @@
+﻿namespace Task10._02;
+
+internal class B
+{
+    private readonly int _index;
+
+    public int Get
+    {
+        get { return _index; }
+    }
+
+    public B(int value)
+    {
+        _index = value;
+    }
+
+    public static B operator +(B a, int b)
+    {
+        return new B(a.Get + b);
+    }
+}
---
+++ c_sharp/Progintro.Part10/Task10.02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task10._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new B(1);
+            var second = new B(2);
+            first += 10;
+            second += 1000;
+            Console.WriteLine($"{first.Get} {second.Get}");
+        }
+    }
+}
