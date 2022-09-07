cb5e0962248c93d80690c4b6a78f9e39dbde2026
sergmelnikov <melnikhov@gmail.com>
2022-09-07 23:31:01 +0300

    Added solution for task 10.06.

---
+++ c_sharp/Progintro.Part10/Task10.06/E.cs
@@ -0,0 +1,9 @@
+﻿namespace Task10._06;
+
+internal class E
+{
+    public int this[int col, int row]
+    {
+        get => col - row;
+    }
+}
---
+++ c_sharp/Progintro.Part10/Task10.06/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task10._06;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var e = new E();
+        Console.WriteLine($"{e[0,0]} {e[100, 100]} {e[-10, -10]}");
+        Console.WriteLine($"{e[1500,7]} {e[7, 55]} {e[-8, -16]}");
+    }
+}
