9ee2824cffae2b4958bf6b618453fbe7192e0d3a
sergmelnikov <melnikhov@gmail.com>
2022-09-07 20:46:38 +0300

    Added solution for task 10.03.

---
+++ c_sharp/Progintro.Part10/Task10.03/D.cs
@@ -0,0 +1,16 @@
+﻿namespace Task10._03;
+
+internal class D
+{
+    public int Prop { private set; get; }
+
+    public D()
+    {
+        Prop = 0;
+    }
+
+    public D(D instance) :this()
+    {
+        Prop = instance.Prop + 1;
+    }
+}
---
+++ c_sharp/Progintro.Part10/Task10.03/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task10._03;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var x = new D();
+        var y = new D(x);
+        var z = new D(y);
+        Console.WriteLine($"{x.Prop} {y.Prop} {z.Prop}");
+    }
+}
