0482533904abe4494d8947a12969fe68dc025438
sergmelnikov <melnikhov@gmail.com>
2022-09-07 23:21:29 +0300

    Added solution for task 10.05.

---
+++ c_sharp/Progintro.Part10/Task10.05/M.cs
@@ -0,0 +1,49 @@
+﻿namespace Task10._05;
+
+internal class M
+{
+    public const int MaxColumns = 3;
+    public const int MaxRows = 3;
+    private int[,] _matrix = new int[MaxColumns, MaxRows]
+    {
+        { 1, 0, 0 },
+        { 0, 1, 0 },
+        { 0, 0, 1 }
+    };
+
+    public int this[int col, int row]
+    {
+        set
+        {
+            if (IsInRange(col, row))
+                _matrix[col - 1, row - 1] = value;
+            else
+                throw new IndexOutOfRangeException();
+        }
+        get
+        {
+            if (IsInRange(col, row))
+                return _matrix[col - 1, row - 1];
+            else
+                throw new IndexOutOfRangeException();
+        }
+    }
+
+    public static M operator +(M a, M b)
+    {
+        var m = new M();
+        for (var i = 1; i <= MaxColumns; i++)
+            for (var j = 1; j <= MaxRows; j++)
+                m[i, j] = a[i, j] + b[i, j];
+        return m;
+    }
+
+    private bool IsInRange(int col, int row)
+    {
+        if ((col >= 1 && col <= 3) &&
+            (row >= 1 && row <= 3))
+            return true;
+        else
+            return false;
+    }
+}
---
+++ c_sharp/Progintro.Part10/Task10.05/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task10._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var m1 = new M();
+            Console.WriteLine($"{m1[1, 1]} {m1[2, 2]} {m1[2, 3]}");
+            var m2 = new M();
+            m1[2, 3] = 7;
+            m2[2, 3] = 350;
+            var m3 = m1 + m2;
+            Console.WriteLine($"{m3[1, 1]} {m3[2, 2]} {m3[2, 3]}");
+        }
+    }
+}
