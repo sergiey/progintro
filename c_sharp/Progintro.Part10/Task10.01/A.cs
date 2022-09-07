3616f15222a589f47f86d2088d5fae5fd7fb6e84
sergmelnikov <melnikhov@gmail.com>
2022-09-07 19:58:15 +0300

    Added soution for task 10.01.

---
+++ c_sharp/Progintro.Part10/Task10.01/A.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class A
+{
+	private readonly int _index;
+
+	public A(int index)
+	{
+		_index = index;
+	}
+
+	public int this[int @value]
+	{
+		get { return @value + _index; }
+	}
+}
---
+++ c_sharp/Progintro.Part10/Task10.01/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task10._01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var first = new A(1);
+        var second = new A(10);
+        Console.WriteLine($"first: {first[100]} {first[200]}");
+        Console.WriteLine($"second: {second[100]} {second[200]}");
+    }
+}
