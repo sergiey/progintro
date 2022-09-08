b9e85c7a1533700cf5be3f5ffb4ceda55dd627d3
sergmelnikov <melnikhov@gmail.com>
2022-09-09 01:02:16 +0300

    Added solution for task 10.14.

---
+++ c_sharp/Progintro.Part10/Task10.14/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task10._14;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var n1 = new Rational(6, 2);
+            Console.WriteLine($"To double {n1.ToDouble()}");
+            var n2 = new Rational(1, 2);
+            Console.WriteLine(
+                $"{n2.Numerator}/{n2.Denominator} - " +
+                $"{n1.Numerator}/{n1.Denominator} = " +
+                $"{(n2 - n1).Numerator}/{(n2 - n1).Denominator}");
+            Console.WriteLine(
+                $"{n2.Numerator}/{n2.Denominator} + " +
+                $"{n1.Numerator}/{n1.Denominator} = " +
+                $"{(n2 + n1).Numerator}/{(n2 + n1).Denominator}");
+            Console.WriteLine(n1.ToLong());
+            var n3 = new Rational(3, 0);
+        }
+        catch (ZeroDenominatorException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ c_sharp/Progintro.Part10/Task10.14/ZeroDenominatorException.cs
@@ -0,0 +1,16 @@
+﻿namespace Task10._14;
+
+[Serializable]
+internal class ZeroDenominatorException : Exception
+{
+    public ZeroDenominatorException()
+    { }
+
+    public ZeroDenominatorException(string message)
+        : base(message)
+    { }
+
+    public ZeroDenominatorException(string message, Exception innerException)
+        : base(message, innerException)
+    { }
+}
