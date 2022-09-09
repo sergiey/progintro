28947e2c12ba188195d871ce3b0b7a94af92e74e
sergmelnikov <melnikhov@gmail.com>
2022-09-09 18:16:21 +0300

    Added solution for task 10.17 (9.10).

---
+++ c_sharp/Progintro.Part10/Task9.10/PascalsTriangle.cs
@@ -0,0 +1,37 @@
+﻿namespace Task9._10;
+
+internal class PascalsTriangle
+{
+    public int LineNumber { get; private set; }
+    public int ItemNumber { get; private set; }
+    public int Combination { get; private set; }
+
+    public PascalsTriangle()
+    {
+        LineNumber = ItemNumber = 0;
+        Combination = 1;
+    }
+
+    public void Next()
+    {
+        if (LineNumber == ItemNumber)
+        {
+            LineNumber++;
+            ItemNumber = 0;
+        }
+        else
+            ItemNumber++;
+        Combination = GetCombination(LineNumber, ItemNumber);
+        Console.Write($"({LineNumber}, {ItemNumber}, {Combination})");
+    }
+
+    private int GetCombination(int lineNumber, int itemNumber)
+    {
+        if (itemNumber > lineNumber)
+            throw new Exception("Item number cannot be more than line number");
+        if (itemNumber == 0 || lineNumber == itemNumber)
+            return 1;
+        return GetCombination(lineNumber - 1, itemNumber - 1) +
+            GetCombination(lineNumber - 1, itemNumber);
+    }
+}
---
+++ c_sharp/Progintro.Part10/Task9.10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task9._10;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        const int MaxItems = 90;
+        var p = new PascalsTriangle();
+        Console.Write($"({p.LineNumber}, {p.ItemNumber}, {p.Combination}), ");
+        for (var i = 0; i < MaxItems; i++)
+        {
+            p.Next();
+            if (i != MaxItems - 1)
+                Console.Write(", ");
+        }
+    }
+}
