66fc87e6ffbcd2fc2e7c8c6af2c731af22472fd8
sergmelnikov <melnikhov@gmail.com>
2022-09-09 13:45:16 +0300

    Added solution for task 10.15.

---
+++ c_sharp/Progintro.Part10/Task10.15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task10._15;
+
+internal class Program
+{
+    static void Swap3<T>(ref T x, ref T y, ref T z)
+    {
+        T tmp = x;
+        x = y;
+        y = z;
+        z = tmp;
+    }
+
+    static void Main(string[] args)
+    {
+        int a = 3, b = 5, c = 1;
+        Console.WriteLine($"a = {a} b = {b} c = {c}");
+        Swap3(ref a, ref b, ref c);
+        Console.WriteLine($"a = {a} b = {b} c = {c}");
+    }
+}
