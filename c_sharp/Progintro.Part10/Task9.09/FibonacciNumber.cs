2ea722a02a3eecec2bd5043fe8862edfc28e0813
sergmelnikov <melnikhov@gmail.com>
2022-09-09 14:07:57 +0300

    Added solution for task 10.17 (9.09)

---
+++ c_sharp/Progintro.Part10/Task9.09/FibonacciNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace Task9._09;
+
+internal class FibonacciNumber
+{
+    private int _prevNumber, _currentNumber;
+
+    public int CurrentNumber { get => _currentNumber; }
+    
+    public FibonacciNumber()
+    {
+        _prevNumber = _currentNumber = 1;
+    }
+
+    public void Next()
+    {
+        var tmp = _currentNumber;
+        _currentNumber = _prevNumber + _currentNumber;
+        _prevNumber = tmp;
+    }
+}
---
+++ c_sharp/Progintro.Part10/Task9.09/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task9._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = new FibonacciNumber();
+            Console.Write($"{n.CurrentNumber} ");
+            for (var i = 0; i <= 10; i++)
+            {
+                Console.Write($"{n.CurrentNumber} ");
+                n.Next();
+            }
+        }
+    }
+}
