f47811360c155ab571306e991358c0930ba84d16
sergmelnikov <melnikhov@gmail.com>
2022-09-09 13:39:14 +0300

    Added solution for task 10.16.

---
+++ c_sharp/Progintro.Part10/Task10.16/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task10._16
+{
+    internal class Program
+    {
+        public static T GetAndZero<T>(ref T b)
+        {
+            T a = b;
+            b = (dynamic)0;
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            int b = 3;
+            int a = GetAndZero(ref b);
+            Console.WriteLine($"a = {a} b = {b}");
+            double d = 3.9;
+            double c = GetAndZero(ref d);
+            Console.WriteLine($"c = {c} d = {d}");
+        }
+    }
+}
